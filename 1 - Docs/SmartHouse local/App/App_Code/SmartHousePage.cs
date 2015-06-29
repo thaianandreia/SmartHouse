a2bac06a8d9fab54a6867f40786f44ed71acb11f
thaianandreia <thaianandreia@hotmail.com>
2015-06-29 13:04:06 -0300

    Projeto Local
    Este projeto é uma cópia que roda em localhost, não sendo necessário
    configurar a máquina para rodar com IP fixo.
    Para testes, não faz comunicação externa.

---
+++ 1 - Docs/SmartHouse local/App/App_Code/SmartHousePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+public class SmartHousePage : System.Web.UI.Page
+{
+    protected string PageName;
+    protected string IP_Energia;
+    protected string IP_Hidraulica;
+    protected string IP_Seguranca;
+
+    public SmartHousePage()
+    {
+        DataAtualSistema = DateTime.Now;
+        DataInicialPadrao = new DateTime(DataAtualSistema.Year, DataAtualSistema.Month, 1);
+        DataFinalPadrao = DataInicialPadrao.AddMonths(1).AddDays(-1);
+
+        PageName = this.Page.ToString().ToUpper().Replace("ASP.", "").Replace("_ASPX", "").Split('_').LastOrDefault();
+        IP_Energia = "192.168.0.202";
+        IP_Hidraulica = "192.168.0.225";
+        IP_Seguranca = "192.168.0.250";
+    }
+
+    public DateTime DataAtualSistema { get; private set; }
+    public DateTime DataInicialPadrao { get; private set; }
+    public DateTime DataFinalPadrao { get; private set; }
+}
+
